7a9a60e090132a4bf91048e42d4ff0306650e41b
Oleg Tsigenov <o.tsigenov@fz-juelich.de>
2016-04-04 15:55:17 +0200

    small typo fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,10 @@ class dcache (
   $package_name          = 'dcache',
   $conf                  = 'undef',
   $dcahe_poolmanagerconf = '/var/lib/dcache/config/poolmanager.conf',
-  $admin_ssh_keys        = 'nodeff',) {
+  $admin_ssh_keys        = 'nodeff',
+  $lock_version          = false,) {
   anchor { 'dcache::start': } ->
-  class { 'dcache::install': #    require => Package['java-1.8.0-openjdk']
-   } ->
+  class { 'dcache::install': require => Package['java-1.8.0-openjdk'] } ->
   class { 'dcache::config': conf => $conf, } ->
   anchor { 'dcache::end': }
 
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,28 +1,27 @@
-class dcache::install ($lock_version=true){
-  $package_ensure      = $dcache::package_ensure
-  $package_name        = $dcache::package_name
-                                                                                                                                                              
-#  notice ("!!!!!!!!!!!!! ${package_ensure} TEST  !!!!!!!!!!!!!!!")
-#        notify { "test":
-#           message => $dcache_version }
- 
-  
-  if  $lock_version {
+class dcache::install ($lock_version = $dcache::lock_version) {
+  $package_ensure = $dcache::package_ensure
+  $package_name = $dcache::package_name
+
+  #  notice ("!!!!!!!!!!!!! ${package_ensure} TEST  !!!!!!!!!!!!!!!")
+  #        notify { "test":
+  #           message => $dcache_version }
+
+
+  if $lock_version {
     file_line { 'dcache_versionlock':
-      ensure  => present,
-      path    => '/etc/yum/pluginconf.d/versionlock.list',
-      line    => "dcache-${package_ensure}.*",
-      match   => '^(\d+:|)dcache-\d+.\d+.\d+.*',
-      before  =>  Package ['dcache']          
+      ensure => present,
+      path   => '/etc/yum/pluginconf.d/versionlock.list',
+      line   => "dcache-${package_ensure}.*",
+      match  => '^(\d+:|)dcache-\d+.\d+.\d+.*',
+      before => Package['dcache'],
     }
   }
-  
-  
+
   $_package_ensure = $package_ensure ? {
     true     => 'present',
     false    => 'purged',
     'absent' => 'purged',
-    default => $package_ensure,
+    default  => $package_ensure,
   }
 
   package { 'dcache':
@@ -31,5 +30,4 @@ class dcache::install ($lock_version=true){
     tag    => 'dcache',
   }
 
-
- }
+}
