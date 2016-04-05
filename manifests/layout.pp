07f199d4cbc20af467254c72ecef5f535f6aea07
Oleg Tsigenov <o.tsigenov@fz-juelich.de>
2016-04-05 11:43:11 +0200

    layout template fix

--- manifests/layout.pp
+++ manifests/layout.pp
@@ -1,37 +1,29 @@
-class dcache::layout  (
-  $l_file = "${dcache::dcache_etc_dir}/layouts/${hostname}.conf",
-  $layout_hash = 'nodeff',
-  $p_setup='nodef',
-   ) {
-    
+class dcache::layout ($l_file = "${dcache::dcache_etc_dir}/layouts/${hostname}.conf", $layout_hash = 'nodeff', $p_setup = 'nodef',) 
+{
   if is_hash($layout_hash) {
     if deep_has_key($layout_hash, 'dCacheDomain') {
-        class { 'dcache::poolmanager': }
+      class { 'dcache::poolmanager': }
     }
-#   notice($admin_ssh_keys )
-    
-    if deep_has_key($layout_hash, 'admin') and $dcache::admin_ssh_keys  != 'nodeff' {
-  
+
+    if deep_has_key($layout_hash, 'admin') and $dcache::admin_ssh_keys != 'nodeff' {
       file { '/etc/dcache/admin/authorized_keys2':
         owner   => $dcache::dcacheuser,
         group   => $dcache::dcachegroup,
         mode    => '0644',
-        content => join( [inline_template( '<%= scope["dcache::admin_ssh_keys"].join("\n") %>'),"\n"],''),
-        before => Class['dcache::poolmanager']
-       }
+        content => join([inline_template('<%= scope["dcache::admin_ssh_keys"].join("\n") %>'), "\n"], ''),
+        before  => Class['dcache::poolmanager']
+      }
     }
   }
-  
-  if ($p_setup !='nodef') {
-     $pools=deep_merge( $p_setup , collect_pools_paths($layout_hash['domains']))
-  }
-  else {
-    $pools=collect_pools_paths($layout_hash['domains'])
+
+  if ($p_setup != 'nodeff') {
+    $pools = deep_merge($p_setup, collect_pools_paths($layout_hash['domains']))
+  } else {
+    $pools = collect_pools_paths($layout_hash['domains'])
   }
 
-  create_resources(dcache::layout::pool, $pools )  
+  create_resources(dcache::layout::pool, $pools)
 
-  
   if ($layout_hash != 'nodeff') {
     file { "${l_file}.puppet":
       owner   => $dcache::dcacheuser,
